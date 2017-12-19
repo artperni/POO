7d6e541be1389cbb57153b37ba2bd8b3889616b9
miguelvitores <34660797+miguelvitores@users.noreply.github.com>
2017-12-19 02:29:35 +0100

    No he hecho nada de valor, tranquilos

--- POO/src/poo/Almacen.java
+++ POO/src/poo/Almacen.java
@@ -40,5 +40,81 @@ public class Almacen {
     
     
     
+    public int totalProductos(){
+       return this.listaProductos.size();
+    }
+    
+    public int caducidadProductos(int dias){
+        int contadorDias=0;
+        for (Producto producto : this.listaProductos){//Recorremos la lista de productos
+            if(producto.isCaducado(dias)){
+                contadorDias++;
+            }
+        }    
+        return contadorDias;
+    }
+    
+    public void estadoProductos(String estado){
+        int contLibre=0,contVendido=0,contReservado=0;
+        for (Producto producto : this.listaProductos){
+            if (producto.isLibre())
+                contLibre++;
+            else if (producto.isReservado())
+                contReservado++;
+            else if (producto.isVendido())
+                contVendido++;
+            else
+                System.out.println("Error en el producto con referencia "+
+                        producto.getReferencia());
+        }
+        System.out.println("\nProductos libres: "+contLibre+"\nProductos vendidos: "+
+                contVendido+"\nProductos reservados: "+contReservado);
+    }
+    
+    public int libreCaducadoProductos(){
+        int contLibCad=0;
+        for (Producto producto : this.listaProductos){
+            if(producto.isCaducado() && producto.isLibre()){
+                contLibCad++;
+            }
+        }
+        return contLibCad;
+    }
+    
+    
+    public void eliminarCaducados(){
+        for (Producto producto : this.listaProductos){
+            if(producto.isCaducado()){
+                this.listaProductos.remove(producto);
+                /*Empiezo a dudar acerca del stock de los productos.
+                Cada producto debería tener una lista de "elementos"
+                iguales entre sí en cuanto a dimensiones pero distintos en
+                estado y fecha de caducidad? Y así cuando eliminemos un producto 
+                caducado estaríamos eliminando un elemento de ese tipo y el 
+                stock de ese producto entonces sí tendría sentido que bajara en 1.*/
+            }
+        }
+    }
+    
+    public void anadir(Producto producto){
+        this.listaProductos.add(producto);
+        //Aquí se me plantea la misma duda
+        
+        
+        
+        
+        
+        
+    }
+    
+    public void ventaProducto(int numeroCorrelativo){
+        Albaran albaran = new Albaran();
+        
+        
+        
+        
+        
+    }
+    
     
 }
